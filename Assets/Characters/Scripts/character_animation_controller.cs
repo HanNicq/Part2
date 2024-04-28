2bb30c13f20855a5ce396ea51d68a7519f4a329a
Qirui Han <Qirui2.Han@live.uwe.ac.uk>
2024-04-28 22:40:32 +0100

    3

---
+++ Assets/Characters/Scripts/character_animation_controller.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("jump");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            animator.SetBool("dance", true);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            animator.SetBool("dance", false);
+        }
+    }
+}
